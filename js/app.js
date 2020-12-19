483b8a1defb826ca4033f9fd8b69f2dba29f52d9
viewtifult <tashanduncan1994@gmail.com>
2020-12-19 10:12:51 +0000

    add sidebar

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ const projectBtn = document.getElementById('projectsBtn');
 projectBtn.addEventListener('click', () =>{
     projects.scrollIntoView({ behavior: 'smooth', block: 'start' });
 })
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "160px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
